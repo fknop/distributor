3fc360a6667388ee97aed1ecddc22eb099d55e12
Florian <florian_knop@hotmail.com>
2019-12-29 14:33:35 +0100

    start jest adapter

---
+++ distributor-js/packages/jest/test/placeholder.test.ts
@@ -0,0 +1,13 @@
+describe('Tests', () => {
+  it('works', () => {
+    expect(1 + 1).toEqual(2)
+  })
+
+  it('works2', () => {
+    expect(1 + 1).toEqual(2)
+  })
+
+  it('works3', () => {
+    expect(1 + 1).toEqual(2)
+  })
+})
